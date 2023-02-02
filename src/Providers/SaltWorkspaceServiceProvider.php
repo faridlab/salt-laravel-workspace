ac05630751573db0bad9a65422918d0b95fc805c
Farid Hidayat <e.faridhidayat@gmail.com>
2023-02-03 01:26:37 +0700

    chore: update service

--- src/Providers/SaltWorkspaceServiceProvider.php
+++ src/Providers/SaltWorkspaceServiceProvider.php
@@ -18,9 +18,9 @@ class SaltWorkspaceServiceProvider extends ServiceProvider
          *
          * Uncomment this function call to make the config file publishable using the 'config' tag.
          */
-        // $this->publishes([
-        //     __DIR__.'/../../config/salt-laravel-workspace.php' => config_path('salt-laravel-workspace.php'),
-        // ], 'config');
+        $this->publishes([
+            __DIR__.'/../../config/salt-laravel-workspace.php' => config_path('workspace.php'),
+        ], 'workspace-config');
 
         /**
          * Routes
@@ -28,7 +28,8 @@ class SaltWorkspaceServiceProvider extends ServiceProvider
          * Uncomment this function call to load the route files.
          * A web.php file has already been generated.
          */
-        // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
+        $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
 
         /**
          * Translations
@@ -81,7 +82,7 @@ class SaltWorkspaceServiceProvider extends ServiceProvider
          * Uncomment the first function call to load the migrations.
          * Uncomment the second function call to make the migrations publishable using the 'migrations' tags.
          */
-        // $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         // $this->publishes([
         //     __DIR__.'/../../database/migrations/' => database_path('migrations')
         // ], 'migrations');
